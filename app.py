8cf44296cb5cd577a6d6342734ee0892ba306175
Ansus <susanna.andrea@gmail.com>
2019-08-27 00:44:18 +0200

    Added multiple page search

--- app.py
+++ app.py
@@ -4,50 +4,62 @@ import pandas as pd
 
 # taking the list of movies from IMdb.com
 url = "https://www.imdb.com/list/ls021348496/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+active_url = url
+response = requests.get(active_url)
+page = 1
+entries = []
 
-css_selector = ".lister-item-content"
-items = soup.select(css_selector)
+while response.status_code == 200:
 
-rows_list = []
-for item in items:
-    header = item.select_one(".lister-item-header").getText().splitlines()
-    rank, title, year = header[1:4]
-    rank = "".join(c for c in rank if c.isdigit())
-    year = "".join(c for c in year if c.isdigit())
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    runtime = item.select_one(".runtime").getText()
-    runtime = "".join(c for c in runtime if c.isdigit())
+    css_selector = ".lister-item-content"
+    items = soup.select(css_selector)
+    if not items:  # if empty
+        break
 
-    genre = item.select_one(".genre").getText()
-    genre_list = [x.strip() for x in genre.split(",")]
+    for item in items:
+        header = item.select_one(".lister-item-header").getText().splitlines()
+        rank, title, year = header[1:4]
+        rank = "".join(c for c in rank if c.isdigit())
+        year = "".join(c for c in year if c.isdigit())
 
-    rating = item.select_one(".ipl-rating-star__rating").getText()
+        runtime = item.select_one(".runtime").getText()
+        runtime = "".join(c for c in runtime if c.isdigit())
 
-    # distiguish the revenue from votes (same structure)
-    chars = ["$", "€"]
-    num_revenue, num_votes = None, None
-    for my_tag in item.find_all(attrs={"name": "nv"}):
-        text = my_tag.getText()
-        digits = "".join(c for c in text if c.isdigit())
-        if any(x in text for x in chars):
-            num_revenue = float(digits)/100
-        else:
-            num_votes = int(digits)
+        genre = item.select_one(".genre").getText()
+        genre_list = [x.strip() for x in genre.split(",")]
 
-    row = {
-        "Rank": int(rank),
-        "Title": title,
-        "Year": int(year),
-        "Minutes": runtime,
-        "Genre1": genre_list[0] if len(genre_list) > 0 else None,
-        "Genre2": genre_list[1] if len(genre_list) > 1 else None,
-        "Genre3": genre_list[2] if len(genre_list) > 2 else None,
-        "Rating": float(rating),
-        "Votes": num_votes,
-        "Revenue": num_revenue
-    }
-    rows_list.append(row)
-df = pd.DataFrame(rows_list)
-print(df["Votes"])
+        rating = item.select_one(".ipl-rating-star__rating").getText()
+
+        # distiguish the revenue from votes (same structure)
+        chars = ["$", "€"]
+        num_revenue, num_votes = None, None
+        for my_tag in item.find_all(attrs={"name": "nv"}):
+            text = my_tag.getText()
+            digits = "".join(c for c in text if c.isdigit())
+            if any(x in text for x in chars):
+                num_revenue = float(digits)/100
+            else:
+                num_votes = int(digits)
+
+        row = {
+            "Rank": int(rank),
+            "Title": title,
+            "Year": int(year),
+            "Minutes": runtime,
+            "Genre1": genre_list[0] if len(genre_list) > 0 else None,
+            "Genre2": genre_list[1] if len(genre_list) > 1 else None,
+            "Genre3": genre_list[2] if len(genre_list) > 2 else None,
+            "Rating": float(rating),
+            "Votes": num_votes,
+            "Revenue": num_revenue
+        }
+        entries.append(row)
+
+    # next page
+    page += 1
+    active_url = url + "?page=" + str(page)
+    response = requests.get(active_url)
+
+df = pd.DataFrame(entries)
